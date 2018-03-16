f75c3083c0fcf93df1f03b89a70c8ec68686a067
adu windows <duuyidong@gmail.com>
2018-03-17 00:14:05 +0800

    fitness2 unfinished

--- fitness1.m
+++ fitness1.m
@@ -1,17 +1,18 @@
 %%多项目总时间
 %%目前求得为多项目总时间，而非目标函数适应度
-function y = fitness1(n, m)
-d = randi([3, 10], 1, n); %% 每个活动持续时间
-numberOfProcess = m * n; %% 活动总数
+
+function y = fitness1(numberOfProcessForSingleProject, numberOfProject) %% n = numberOfProcessForSingleProject; m = numberOfProject
+allProcessTimeList = randi([3, 10], 1, numberOfProcessForSingleProject); %% 每个活动持续时间
+numberOfProcess = numberOfProject * numberOfProcessForSingleProject; %% 活动总数
 multiProjectTime = 0;
-for j = 1:m %% 项目迭代
+for j = 1:numberOfProject %% 项目迭代
     singleProjectTime = 0;
-    for i = 1:n %% 活动迭代
-        singleProjectTime = singleProjectTime + d(i);
+    for i = 1:numberOfProcessForSingleProject %% 活动迭代
+        singleProjectTime = singleProjectTime + allProcessTimeList(i);
     end
     multiProjectTime = multiProjectTime + singleProjectTime;
 end
-y = min(multiProjectTime - sd(numberOfProcess));
+y = min(multiProjectTime - sd(numberOfProcess) - dj(numberOfProject, numberOfProcessForSingleProject));
 %%y = multiProjectTime;
 end
 
@@ -21,4 +22,16 @@ for n = 1:numberOfProcess
     saveTime = saveTime + 2*rand();
 end
 y = saveTime;
+end
+
+function y = dj(numberOfProject, numberOfProcessForSingleProject)
+saveTime = 0;
+for m = 1:numberOfProject
+    saveTime = saveTime + djTime(numberOfProcessForSingleProject);
+end
+y = saveTime;
+end
+
+function y = djTime(numberOfProcessForSingleProject)
+y = ( numberOfProcessForSingleProject - 1 ) * 3 *rand();
 end
--- fitness2.m
+++ fitness2.m
@@ -1,5 +1,15 @@
-%%fitness2 for schedule
-function y = fitness2()
-syms k
-y = syssum()
+%%目标函数：利润最大化
+
+function y = fitness2(numberOfProject)
+ multiProjectProfit = 0;
+ for j = 1: numberOfProject
+    multiProjectProfit = multiProjectProfit + singleProjectProfit();
+ end
+ y = nultiProjectProfit;
+end
+
+function y = singleProjectProfit(payForContract)
+prePay = 0.5 * rand() * payForContract;
+k = randi(9) + 1;  %% 付款次数
+y = prePay + 
 end
